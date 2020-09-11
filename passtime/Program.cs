b5c7680ed498fe244f930eaf5e3f289ae36a2541
kxtemas <71146102+kxtemas@users.noreply.github.com>
2020-09-11 10:22:33 -0700

    Add files via upload
    read and write file in new file

---
+++ passtime/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace passtime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = System.IO.File.ReadAllText(@"C:\WriteText.txt");
+            System.Console.WriteLine("Contents of WriteText.txt = {0}", text);
+            
+            string[] lines = System.IO.File.ReadAllLines(@"C:\WriteLines2.txt");
+            System.Console.WriteLine("Contents of WriteLines2.txt = ");
+        
+            foreach (string line in lines)
+            {
+
+                Console.WriteLine("\t" + line);
+            }
+            Console.WriteLine("Press any key to exit.");
+            System.Console.ReadLine();
+        }
+    }
+}
